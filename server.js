8e8681ec8b469d62212083114e5f962481fa7bb6
MicahBear <micah.b.elm@gmail.com>
2022-08-21 11:38:02 -0700

    update to server file

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = 2121
+const PORT = process.env.PORT || 5000
 require('dotenv').config()
 
 
 app.use(express.static('public'))
 
-app.listen(process.env.PORT || PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
