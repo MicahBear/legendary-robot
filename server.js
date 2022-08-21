4bac073f730e017faeb5c07fb5bf39e020519527
MicahBear <micah.b.elm@gmail.com>
2022-08-21 14:42:21 -0700

    update to express static folder

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000
+const PORT = 2121
 require('dotenv').config()
 
 
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'))
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
+    console.log(__dirname)
+    console.log(__filename)
 })
