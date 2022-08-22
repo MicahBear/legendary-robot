2f1cb2c6a2de2b6a35f994040d6320cdedcba116
MicahBear <micah.b.elm@gmail.com>
2022-08-22 08:51:26 -0700

    beginning logic for listener on buttons

--- public/app.js
+++ public/app.js
@@ -1,1 +1,9 @@
 document.querySelector('#calculator-window')
+document.querySelector('#one').addEventListener('click', numbers)
+
+
+function numbers() {
+    const content = document.querySelector('[data-action]').textContent
+    console.log(content)
+}
+console.log(numbers)
